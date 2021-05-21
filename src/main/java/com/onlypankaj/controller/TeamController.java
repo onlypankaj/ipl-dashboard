c970fdae2ef937ed720ef12a24f4c0c761cb3b7f
onlypankaj <onlypankaj@gmail.com>
2021-05-22 00:49:31 +0530

    Added Backend Match API

--- src/main/java/com/onlypankaj/controller/TeamController.java
+++ src/main/java/com/onlypankaj/controller/TeamController.java
@@ -1,12 +1,13 @@
 package com.onlypankaj.controller;
 
+import com.onlypankaj.model.Match;
 import com.onlypankaj.model.Team;
 import com.onlypankaj.repository.MatchRepository;
 import com.onlypankaj.repository.TeamRespository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -28,5 +29,17 @@ public class TeamController {
 
         System.out.println("Team UI called" + team);
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+        LocalDate startDate = LocalDate.of(year, 1,1 );
+        LocalDate endDate = LocalDate.of(year+1, 1,1 );
+
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName,startDate,endDate);
+
+//        return this.matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(
+//                teamName,startDate,endDate,
+//                teamName, startDate,endDate);
     }
 }
