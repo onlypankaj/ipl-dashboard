90bb4a1eff0067604dd0d48e10c8079c6e0d729c
onlypankaj <onlypankaj@gmail.com>
2021-05-24 00:52:19 +0530

    Issue:Fixed: tied match cases with different colour

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -7,11 +7,19 @@ export const MatchSmallCard = ({ teamName, match }) => {
     const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
     const otherTeamRoute = `/teams/${otherTeam}`;
     const isMatchWon = teamName === match.matchWinner;
+    const isTie = match.result == 'tie' ? true : false;
+    var cardColur = isTie ? 'MatchDetailCard tie-card' : isMatchWon ? 'MatchDetailCard won-card' : 'MatchDetailCard lost-card';
+    var matchResult = "";
+    if(isTie){
+        matchResult = "There was a tie between " + match.team1 + " and " + match.team2;  
+    } else {
+        matchResult = match.matchWinner + " won by " + match.resultMargin + " " + match.result;
+    }
     return (
-        <div className={isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'}>
+        <div className={cardColur}>
             <span className="vs">vs</span>
             <h3><Link to={otherTeamRoute}>{otherTeam}</Link></h3>
-            <p className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result}</p>
+            <p className="match-result">{matchResult}</p>
         </div>
     );
 }
