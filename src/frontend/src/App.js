2e05ed9115a8e34ac6c9c7fd7a29480534052e92
onlypankaj <onlypankaj@gmail.com>
2021-05-23 23:48:23 +0530

    Issue: reverted: added nav bar links: But Toolbar component not removed

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,21 +3,19 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 import { MatchPage } from './pages/MatchPage';
 import { HomePage } from './pages/HomePage';
-import  Toolbar from "./components/Toolbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Toolbar></Toolbar>
         <Switch>
-          <Route exactly path="/teams/:teamName/matches/:year">
+          <Route path="/teams/:teamName/matches/:year">
             <MatchPage />
           </Route>
-          <Route exactly path="/teams/:teamName">
+          <Route path="/teams/:teamName">
             <TeamPage />
           </Route>
-          <Route exactly path="/">
+          <Route path="/">
             <HomePage/>
           </Route>
         </Switch>
